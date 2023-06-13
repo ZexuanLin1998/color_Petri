17833021edcfbf5063546e13ff9cfec28194f698
阿泽 <lzx886200@163.com>
2023-06-13 16:50:47 +0800

    设计读csv与一维数组的函数

--- git_平行有色执行网Petri网25x86/git_平行有色执行网Petri网25x86/read_class_pt.h
+++ git_平行有色执行网Petri网25x86/git_平行有色执行网Petri网25x86/read_class_pt.h
@@ -1,6 +1,11 @@
 #pragma once
 constexpr auto class_Place = ".\\data\\3Color\\coloured_pn.json";
 constexpr auto ModelPath = "C:\\Users\\Public\\model\\model25x81.pt";
+constexpr auto Place_info = "C:\\Users\\Public\\model\\place_info.csv";
+constexpr auto C_pre = "C:\\Users\\Public\\model\\C_pre.csv";
+constexpr auto C_post = "C:\\Users\\Public\\model\\C_post.csv";
+constexpr auto C_delay = "C:\\Users\\Public\\model\\C_delay.csv";
+constexpr auto c_stack0 = "C:\\Users\\Public\\model\\c_stack0.csv";
 
 #include<iostream>
 #include<string>
@@ -19,4 +24,56 @@ inline string read_json(const char* path) {
 	cout << str << endl;
 	file_json.close();
 	return str;
+}
+
+/*读csv文件*/
+
+inline vector<vector<int>> read_csv(const char* path)
+{
+	std::ifstream file_csv(path);
+	std::string lineStr;
+	std::vector<vector<int>> datas;
+	if (!file_csv.is_open())
+	{
+		std::cout << "fail to open file" << std::endl;
+		exit(1);
+	}
+	while (getline(file_csv, lineStr))
+	{
+		vector<string>row;
+		vector<int>temp;
+		size_t pos = 0;
+		string token;
+		while ((pos = lineStr.find(",")) != string::npos)
+		{
+			token = lineStr.substr(0, pos);
+			row.push_back(token);
+			lineStr.erase(0, pos + 1);
+		}
+		row.push_back(lineStr);
+		for (int i = 0;i < row.size();i++)
+		{
+			temp.push_back(atoi(row[i].c_str()));
+		}
+		datas.push_back(temp);
+	}
+	return datas;
+}
+
+
+/*读一维数组*/
+inline vector<int>read_vector(const char* path) {
+	vector<int> result;
+	ifstream file(path);
+	if (!file.is_open()) {
+		cout << "can not open file :" << path << endl;
+		return {};
+	}
+	int num;
+	while (file >> num)
+	{
+		result.push_back(num);
+	}
+	file.close();
+	return result;
 }
